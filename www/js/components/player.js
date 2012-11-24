151db6478ff9d5d7d10af1667ea2b254e7fe40e8
cypi <epicyr@gmail.com>
2012-11-24 12:22:34 +0100

    app.js refactor + conf.js

--- www/js/components/player.js
+++ www/js/components/player.js
@@ -1,31 +1,35 @@
-Crafty.c('Onibi', {
-    init: function () {
-      this.requires('Mouse')
-          .areaMap([0, 0], [0, CONF.onibi.size], [CONF.onibi.size, CONF.onibi.size], [CONF.onibi.size, 0]);
-      this.bind('Click', function () {
-        var selected = Crafty('Selected');
-        if (selected.length) {
-          for (var i = 0; i < selected.length; i++) {
-            Crafty(selected[i]).removeComponent('Selected');
+require(['lib/crafty','conf'], function(crafty, CONF) {
+
+  Crafty.c('Onibi', {
+      init: function () {
+        this.requires('Mouse')
+            .areaMap([0, 0], [0, CONF.onibi.size], [CONF.onibi.size, CONF.onibi.size], [CONF.onibi.size, 0]);
+        this.bind('Click', function () {
+          var selected = Crafty('Selected');
+          if (selected.length) {
+            for (var i = 0; i < selected.length; i++) {
+              Crafty(selected[i]).removeComponent('Selected');
+            }
           }
-        }
-        this.addComponent('Selected');
-      });
+          this.addComponent('Selected');
+        });
 
-      this.requires('Collision').collision();
+        this.requires('Collision').collision();
 
-    },
-    move: function(toX, toY) {
-      toX -= this.w / 2;
-      toY -= this.h / 2;
+      },
+      move: function(toX, toY) {
+        toX -= this.w / 2;
+        toY -= this.h / 2;
 
-      var dx = this.x - toX,
-          dy = this.y - toY,
-          dist = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2)),
-          speed = Math.round(dist / CONF.onibi.speed);
+        var dx = this.x - toX,
+            dy = this.y - toY,
+            dist = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2)),
+            speed = Math.round(dist / CONF.onibi.speed);
 
-      this.tween({ x: toX, y: toY }, speed);
+        this.tween({ x: toX, y: toY }, speed);
 
-      return this;
-    }
+        return this;
+      }
+  });
+
 });
